dd8aeb1d68d8788f59fd5a84d119a486c3800a0c
HWilliamGo <williamintoandroid@outlook.com>
2021-05-10 21:00:06 +0800

    init

---
+++ app/src/main/java/com/hwilliamgo/livertmp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.hwilliamgo.livertmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    ScreenLive screenLive;
+    String url = "rtmp://sendtc3a.douyu.com/live/6458908rdXEGQcTb?wsSecret=7cfbe9255a6037560ba3fb8c2d68a97e&wsTime=6097dab5&wsSeek=off&wm=0&tw=0&roirecognition=0&record=flv&origin=tct";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+    }
+    public boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+        }
+        return false;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+//         mediaProjection--->产生录屏数据
+            mediaProjection = mediaProjectionManager.getMediaProjection
+                    (resultCode, data);
+            screenLive = new ScreenLive();
+            screenLive.startLive(url, mediaProjection);
+        }
+    }
+
+
+
+    public void startLive(View view) {
+        this.mediaProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Intent captureIntent = mediaProjectionManager.createScreenCaptureIntent();
+        startActivityForResult(captureIntent, 100);
+    }
+}
